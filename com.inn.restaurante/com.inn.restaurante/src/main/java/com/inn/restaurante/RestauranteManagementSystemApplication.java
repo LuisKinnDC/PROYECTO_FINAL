7a7128a89de9e9ae6040f94ccd1372c80b9eaa91
Luis Kinn DC <137831123+LuisKinnDC@users.noreply.github.com>
2025-01-08 21:50:36 -0500

    Add files via upload

---
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/RestauranteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteManagementSystemApplication.class, args);
+	}
+
+}
---
+++ com.inn.restaurante/com.inn.restaurante/src/test/java/com/inn/restaurante/RestauranteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
