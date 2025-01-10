c544300b9aec8a9742dded9fdba9bffa83b1b7ef
Luis Kinn DC <luiskinndc@gmail.com>
2025-01-10 12:20:41 -0500

    agregue dos  carpetas el cual son service y serviceImpl

---
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/service/BillService.java
@@ -0,0 +1,14 @@
+package com.inn.restaurante.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface BillService {
+
+    ResponseEntity<String> generateReport(Map<String, Object> requestMap);
+
+    ResponseEntity<List<Bill>> getBills();
+
+    ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap);
+
+    ResponseEntity<String> deleteBill(Integer id);
+}
