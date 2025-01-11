dd4275399c80ce0637347ce9c2ff0c0378a53e74
Luis Kinn DC <luiskinndc@gmail.com>
2025-01-10 20:59:27 -0500

    se creo 3 interfaces mas en la carpeta de service, los cuales son DasboarService, ProductService y UserService

---
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/service/DashboardService.java
@@ -0,0 +1,9 @@
+package com.inn.restaurante.service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface DashboardService {
+    ResponseEntity<Map<String, Object>> getCount();
+}
