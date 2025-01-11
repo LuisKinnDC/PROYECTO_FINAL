207a167d03f8aec67c301a8b4f4d611f238826ea
Luis Kinn DC <luiskinndc@gmail.com>
2025-01-10 21:50:20 -0500

    agregue la interface de userservice

---
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/service/UserService.java
@@ -0,0 +1,22 @@
+package com.inn.restaurante.service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface UserService {
+
+    ResponseEntity<String> signUp(Map<String, String> requestMap);
+
+    ResponseEntity<String> login(Map<String, String> requestMap);
+
+    ResponseEntity<List<UserWrapper>> getAllUser();
+
+    ResponseEntity<String> update(Map<String, String> requestMap);
+
+    ResponseEntity<String> checkToken();
+
+    ResponseEntity<String> changePassword(Map<String, String> requestMap);
+
+    ResponseEntity<String> forgotPassword(Map<String, String> requestMap);
+}
