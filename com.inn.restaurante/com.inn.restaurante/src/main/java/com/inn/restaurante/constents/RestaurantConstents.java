673c00e1f58840018052517046c986b15e93a7f0
23luz0 <luzbritanyfloresquispe@gmail.com>
2025-01-11 21:43:50 -0500

    En la clase RestaurantConstents incorporé códigos para gestionar mensajes de error y constantes del sistema

--- com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/constents/RestaurantConstents.java
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/constents/RestaurantConstents.java
@@ -1,4 +1,12 @@
 package com.inn.restaurante.constents;
 
 public class RestaurantConstents {
+
+    public static final String SOMETHING_WENT_WRONG= "Something Went Wrong.";
+
+    public static final String INVALID_DATA = "Invalid Data.";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "C:\\Users\\USUARIO\\Documentos";
 }
