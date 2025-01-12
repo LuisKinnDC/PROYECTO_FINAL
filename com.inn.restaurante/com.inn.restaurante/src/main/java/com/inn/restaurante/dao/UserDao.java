91602d69ac0a719cffbdf4722cb9b7545dd440a4
23luz0 <luzbritanyfloresquispe@gmail.com>
2025-01-11 21:31:09 -0500

    En la interfaz UserDao incorporé códigos para operaciones de usuarios, incluyendo búsqueda por email y actualización de estado

--- com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/dao/UserDao.java
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/dao/UserDao.java
@@ -1,4 +1,25 @@
 package com.inn.restaurante.dao;
 
-public interface UserDao {
+import com.inn.restaurante.POJO.User;
+import com.inn.restaurante.wrapper.UserWrapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+
+    User findByEmailId(@Param("email") String email);
+
+    List<UserWrapper> getAllUser();
+
+    List<String> getAllAdmin();
+
+    @Transactional
+    @Modifying
+    Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
+
+    User findByEmail(String email);
 }
