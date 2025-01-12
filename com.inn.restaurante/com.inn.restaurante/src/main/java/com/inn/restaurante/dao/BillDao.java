d0483205acdb0dc832323be04d06265d68e500c4
23luz0 <luzbritanyfloresquispe@gmail.com>
2025-01-11 21:36:31 -0500

    En la interfaz BillDao incorporé códigos para operaciones relacionadas con facturas, incluyendo consultas por nombre de usuario

--- com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/dao/BillDao.java
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/dao/BillDao.java
@@ -1,4 +1,14 @@
 package com.inn.restaurante.dao;
 
-public interface BillDao {
+import com.inn.restaurante.POJO.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BillDao extends JpaRepository<Bill, Integer> {
+
+    List<Bill> getAllBills();
+
+    List<Bill> getBillByUserName(@Param("username") String username);
 }
