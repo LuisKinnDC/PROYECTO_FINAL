68f3fd9bb8669bde0eab39a9a8af2e82ae290f5f
juan <juanprogrammer10@hotmail.com>
2025-01-12 22:05:43 -0500

    aqui se creo las interfaces del rest y asi mismo  las clases que se implementan

---
+++ com.inn.restaurante/com.inn.restaurante/src/main/java/com/inn/restaurante/restImpl/DashboardRestImpl.java
@@ -0,0 +1,25 @@
+package com.inn.restaurante.restImpl;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inn.restaurante.rest.DashboardRest;
+import com.inn.restaurante.service.DashboardService;
+
+@RestController
+public class DashboardRestImpl implements DashboardRest {
+
+
+    
+    @Autowired
+    DashboardService dashboardService;
+
+    @Override
+    public ResponseEntity<Map<String, Object>> getCount() {
+        return dashboardService.getCount();
+    }
+
+}
